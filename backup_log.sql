f9be58cc38ddd45f60bea7719258c497f9bacb78
Bruno Silva <brunop300403@gmail.com>
2026-03-10 12:12:22 -0500

    Update database name and backup path in SQL script

--- backup_log.sql
+++ backup_log.sql
@@ -2,8 +2,8 @@ DECLARE @DatabaseName NVARCHAR(100)
 DECLARE @BackupPath NVARCHAR(500)
 DECLARE @FileName NVARCHAR(500)
 
-SET @DatabaseName = 'SeuBanco'
-SET @BackupPath = 'C:\Backup\Logs\'
+SET @DatabaseName = 'Nome_do_seu_banco'
+SET @BackupPath = 'Disco_de_armazenamento_backups'
 
 SET @FileName = @BackupPath + @DatabaseName + '_' +
 CONVERT(VARCHAR, GETDATE(), 112) + '.trn'
